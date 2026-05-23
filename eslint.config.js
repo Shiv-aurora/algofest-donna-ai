ae2223e23af6795ba4f554d2aba87c3aff4aaacb
Shiv-aurora <shivamarora.sa05@gmail.com>
2026-05-23 09:05:00 +0530

    chore(frontend): update dependencies and lint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'landing page/**']),
+  globalIgnores(['dist', 'landing page/**', 'algo-service/.venv/**']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
